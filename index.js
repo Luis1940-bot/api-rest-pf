bfe8db48db8428431b8313180bac28d8c9d0deab
Luis Gimenez <luisconsultor@outlook.com>
2022-06-20 11:04:17 -0300

    first commit

--- index.js
+++ index.js
@@ -20,13 +20,13 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 require("dotenv").config();
-
+const PORT = process.env.PORT;
 // Syncing all the models at once.
 //modifica => alter: true
 //desde cero => force:true
 conn.sync({}).then(() => {
-  server.listen(process.env.PORT, () => {
-    console.log("%s listening at 3001"); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log("%s listening at " + PORT); // eslint-disable-line no-console
   });
 });
 
