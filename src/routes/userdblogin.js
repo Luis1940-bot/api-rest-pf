027342f9df0ecdf1984faab94a9eabf80bd21336
Luis Gimenez <luisconsultor@outlook.com>
2022-06-21 21:36:58 -0300

    commit se agrega cors en usbloggins

--- src/routes/userdblogin.js
+++ src/routes/userdblogin.js
@@ -25,123 +25,129 @@ router.use(
   })
 );
 
-router.post("/userdblogin", userValidShortReg(), validate, async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    const userFound = await db.Users.findOne({
-      where: {
-        email: email,
-      },
-      include: [
-        {
-          model: db.Cities,
-          attributes: ["name"],
+router.post(
+  "/userdblogin",
+  cors(),
+  userValidShortReg(),
+  validate,
+  async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      const userFound = await db.Users.findOne({
+        where: {
+          email: email,
         },
-        {
-          model: db.States,
-          attributes: ["name"],
-          //required: true,
-        },
-        {
-          model: db.Countries,
-          attributes: ["name"],
-          //required: true,
-        },
-        {
-          model: db.Professionals,
-          // required: true,
-        },
-      ],
-      raw: true,
-    });
-
-    if (!userFound) {
-      return res.status(401).json({
-        error: "User not found",
+        include: [
+          {
+            model: db.Cities,
+            attributes: ["name"],
+          },
+          {
+            model: db.States,
+            attributes: ["name"],
+            //required: true,
+          },
+          {
+            model: db.Countries,
+            attributes: ["name"],
+            //required: true,
+          },
+          {
+            model: db.Professionals,
+            // required: true,
+          },
+        ],
+        raw: true,
       });
-    }
 
-    if (userFound.active === 0) {
-      return res.status(401).json({ error: "User is inactive" });
-    }
-    if (userFound.validated_email === 0) {
-      return res
-        .status(401)
-        .json({ error: "User email is not validated", id: userFound.id });
-    }
-
-    bcrypt.compare(password, userFound.password, (err, result) => {
-      if (err) {
+      if (!userFound) {
         return res.status(401).json({
-          error: "Error checking password",
+          error: "User not found",
         });
       }
-      if (!result) {
-        return res.status(401).json({ error: "Wrong password" });
-      }
-      // console.log("SEQUELIZE userFound", userFound);
-      // TOKEN JWT
-      const userInfoFront = {
-        id: userFound.id,
-        email: userFound.email,
-        name: userFound.name,
-        lastname: userFound.lastname,
-        phone: userFound.phone,
-        address: userFound.address,
-        // countryId: userFound.countryId,
-        countryName: userFound["country.name"],
-        city: userFound["city.name"],
-        state: userFound["state.name"],
-        professionalId: userFound["professionals.id"],
-        professionalTuition: userFound["professionals.tuition"],
-        professionalTrainings: userFound["professionals.trainings"],
-        professionalActive: userFound["professionals.active"],
-        professionalPhoto: userFound["professionals.photo"],
-        professionalCvu: userFound["professionals.cvu"],
-        professionalBalance: userFound["professionals.balance"],
-        professionalCreatedAt: userFound["professionals.createdAt"],
-        professionalUpdatedAt: userFound["professionals.updatedAt"],
-      };
-      // console.log("userInfoFront", userInfoFront);
-      const tokenFront = jwt.sign(userInfoFront, process.env.TOKENKEY, {
-        expiresIn: "3h",
-      });
-      const tokenBack = jwt.sign({ id: userFound.id }, process.env.TOKENKEY, {
-        expiresIn: "3h",
-      });
 
-      // COOKIE BACKEND
-      res.cookie("userBackend", tokenBack, {
-        expires: new Date(Date.now() + 3 * 60 * 60 * 1000), //3 hours expiration
-        httpOnly: true,
-        sameSite: none,
-        secure: true,
-      });
-      // COOKIE FRONTEND
-      res.cookie(
-        "SessionUserClickCare",
-        { userId: userFound.id },
-        {
+      if (userFound.active === 0) {
+        return res.status(401).json({ error: "User is inactive" });
+      }
+      if (userFound.validated_email === 0) {
+        return res
+          .status(401)
+          .json({ error: "User email is not validated", id: userFound.id });
+      }
+
+      bcrypt.compare(password, userFound.password, (err, result) => {
+        if (err) {
+          return res.status(401).json({
+            error: "Error checking password",
+          });
+        }
+        if (!result) {
+          return res.status(401).json({ error: "Wrong password" });
+        }
+        // console.log("SEQUELIZE userFound", userFound);
+        // TOKEN JWT
+        const userInfoFront = {
+          id: userFound.id,
+          email: userFound.email,
+          name: userFound.name,
+          lastname: userFound.lastname,
+          phone: userFound.phone,
+          address: userFound.address,
+          // countryId: userFound.countryId,
+          countryName: userFound["country.name"],
+          city: userFound["city.name"],
+          state: userFound["state.name"],
+          professionalId: userFound["professionals.id"],
+          professionalTuition: userFound["professionals.tuition"],
+          professionalTrainings: userFound["professionals.trainings"],
+          professionalActive: userFound["professionals.active"],
+          professionalPhoto: userFound["professionals.photo"],
+          professionalCvu: userFound["professionals.cvu"],
+          professionalBalance: userFound["professionals.balance"],
+          professionalCreatedAt: userFound["professionals.createdAt"],
+          professionalUpdatedAt: userFound["professionals.updatedAt"],
+        };
+        // console.log("userInfoFront", userInfoFront);
+        const tokenFront = jwt.sign(userInfoFront, process.env.TOKENKEY, {
+          expiresIn: "3h",
+        });
+        const tokenBack = jwt.sign({ id: userFound.id }, process.env.TOKENKEY, {
+          expiresIn: "3h",
+        });
+
+        // COOKIE BACKEND
+        res.cookie("userBackend", tokenBack, {
           expires: new Date(Date.now() + 3 * 60 * 60 * 1000), //3 hours expiration
-          httpOnly: false,
+          httpOnly: true,
           sameSite: none,
           secure: true,
-        }
-      );
+        });
+        // COOKIE FRONTEND
+        res.cookie(
+          "SessionUserClickCare",
+          { userId: userFound.id },
+          {
+            expires: new Date(Date.now() + 3 * 60 * 60 * 1000), //3 hours expiration
+            httpOnly: false,
+            sameSite: none,
+            secure: true,
+          }
+        );
 
-      // RESPONSE
-      res.status(200).json({
-        message: "Login success",
-        token: tokenFront,
-        // userInformation: userInfoFront,
+        // RESPONSE
+        res.status(200).json({
+          message: "Login success",
+          token: tokenFront,
+          // userInformation: userInfoFront,
+        });
       });
-    });
-  } catch (e) {
-    return res.status(401).json({
-      error: e,
-    });
+    } catch (e) {
+      return res.status(401).json({
+        error: e,
+      });
+    }
   }
-});
+);
 
 //RUTA SOLO PARA PROBAR LECTURA DE COOKIE con jwt.verify
 router.get("/cookieBackendRead", (req, res) => {
