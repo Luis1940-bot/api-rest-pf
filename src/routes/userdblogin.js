baec400195f52456a14a30b71f65f9dde41cd939
Luis Gimenez <luisconsultor@outlook.com>
2022-06-22 19:05:15 -0300

    commit  tk

--- src/routes/userdblogin.js
+++ src/routes/userdblogin.js
@@ -112,32 +112,31 @@ router.post("/userdblogin", userValidShortReg(), validate, async (req, res) => {
       });
 
       // COOKIE BACKEND
-      // res.cookie("userBackend", tokenBack, {
-      //   expires: new Date(Date.now() + 3 * 60 * 60 * 1000), //3 hours expiration
-      //   httpOnly: true,
-      //   sameSite: "none",
-      //   secure: true,
-      // });
+      res.cookie("userBackend", tokenBack, {
+        expires: new Date(Date.now() + 3 * 60 * 60 * 1000), //3 hours expiration
+        httpOnly: true,
+        sameSite: "none",
+        secure: true,
+      });
       // COOKIE FRONTEND
       res.cookie(
         "SessionUserClickCare",
-        // { userId: userFound.id },
-        tokenFront,
+        { userId: userFound.id },
         {
           domain: "*", //domain, // "https://deploy-click-care.vercel.app/",
           expires: new Date(Date.now() + 3 * 60 * 60 * 1000), //3 hours expiration
-          httpOnly: true,
-          sameSite: false,
+          //httpOnly: true,
+          //sameSite: false,
           secure: true,
         }
       );
 
       // RESPONSE
-      // res.status(200).json({
-      //   message: "Login success",
-      //   token: tokenFront,
-      //   // userInformation: userInfoFront,
-      // });
+      res.status(200).json({
+        message: "Login success",
+        token: tokenFront,
+        // userInformation: userInfoFront,
+      });
     });
   } catch (e) {
     return res.status(401).json({
