1a79263d67371f2b753a0b876c251a45c450f43f
Luis Gimenez <luisconsultor@outlook.com>
2022-06-24 20:09:19 -0300

    commit userId: idToken

--- src/routes/userdblogin.js
+++ src/routes/userdblogin.js
@@ -140,7 +140,7 @@ router.post("/userdblogin", userValidShortReg(), validate, async (req, res) => {
       res.status(200).json({
         message: "Login success",
         token: tokenFront,
-        // userInformation: userInfoFront,
+        userId: idToken,
       });
     });
   } catch (e) {
