a5f33019ab8dc76c173e65d66b57ef4f4f7899a0
Luis Gimenez <luisconsultor@outlook.com>
2022-06-25 21:58:13 -0300

    commit const db = require("../db.js");

--- src/routes/message.js
+++ src/routes/message.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 //const axios = require("axios");
 //const { Message } = require("../db");
+const db = require("../db.js");
 
 const cors = require("cors");
 router.use(
